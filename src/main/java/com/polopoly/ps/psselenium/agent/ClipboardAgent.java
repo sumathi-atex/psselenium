caa19d72b4db3c99a14494bbb77257171076cfa9
Mattias Johansson <mattias.johansson@atex.com>
2013-03-19 10:51:23 +0100

    Added overloaded paste method to the clipboardAgent to allow for paste in ajax lists

--- src/main/java/com/polopoly/ps/psselenium/agent/ClipboardAgent.java
+++ src/main/java/com/polopoly/ps/psselenium/agent/ClipboardAgent.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 /**
- * This agent interacts with the clipboard in the Polopoly Admin GUI.  
+ * This agent interacts with the clipboard in the Polopoly Admin GUI.
  */
 public class ClipboardAgent {
 
@@ -20,14 +20,14 @@ public class ClipboardAgent {
      * @throws Exception
      */
     public ClipboardAgent copyOpenedContent() throws Exception {
-        
+
         WebDriver webDriver = guiAgent.getWebDriver();
         webDriver.findElement(
                 By.xpath("//div[contains(@class,'content-info')]/button[contains(@class, 'clipboard')]")).click();
-        
+
         return this;
     }
-    
+
     /**
      * Copy a specific content into the clipboard present in the current frame pane
      * @param contentIdString the content's id to copy
@@ -36,43 +36,61 @@ public class ClipboardAgent {
      */
     public ClipboardAgent copyContent(String contentIdString) throws Exception {
         WebDriver webDriver = guiAgent.getWebDriver();
-        webDriver.findElement(By.xpath("//button[contains(@class, 'clipboard') and contains(@onclick, '" 
+        webDriver.findElement(By.xpath("//button[contains(@class, 'clipboard') and contains(@onclick, '"
                        + contentIdString + "')]")).click();
         return this;
     }
-    
+
     /**
-     * Paste the current content in the clipboard into a specific target
+     * Paste the current content in the clipboard into a specific target,
+     * will wait on a page reload
      * @param target the target's label to paste in
      * @return this agent
      * @throws Exception
      */
-    public ClipboardAgent pasteContent(String target) 
+    public ClipboardAgent pasteContent(String target)
         throws Exception {
-        
+        return pasteContent(target, false);
+    }
+
+    /**
+     * Paste the current content in the clipboard into a specific target,
+     * will wait for ajax refresh
+     * @param target the target's label to paste in
+     * @return this agent
+     * @throws Exception
+     */
+    public ClipboardAgent pasteContent(String target, boolean isAjaxList)
+        throws Exception {
+
         String pasteSelectorPrefix = "//h2[contains(text(),'"+target+"')]";
         String pasteSelectorSuffix = "/button[contains(@class, 'clipboard') and contains(@title, 'Paste')]";
-        
+
         // Slots
         String pasteSelector = pasteSelectorPrefix + "/../" + pasteSelectorSuffix;
-        
+
         WebDriver webDriver = guiAgent.getWebDriver();
-        
+
         if (webDriver.findElements(By.xpath(pasteSelector)).isEmpty()) {
             // Content lists
             pasteSelector = pasteSelectorPrefix + pasteSelectorSuffix;
         }
-        
+
         if (webDriver.findElements(By.xpath(pasteSelector)).isEmpty()) {
            pasteSelector = "//fieldset[contains(@class, '"+target+"']/" + pasteSelectorSuffix;
         }
-        
+
         webDriver.findElement(By.xpath(pasteSelector)).click();
-        guiAgent.agentWait().waitForPageToLoad();
-        
+
+        if(isAjaxList) {
+            guiAgent.agentWait().waitForAjaxPageToLoad();
+        } else {
+            guiAgent.agentWait().waitForPageToLoad();
+        }
+
         return this;
     }
-    
+
     /**
      * Returns the size of clipboard
      * @return the size of the clipboard
